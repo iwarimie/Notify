9f67527d04254badc08406898c9731b94a2b570d
coderdivi <khosee6@gmail.com>
2023-05-05 06:34:29 +0100

    event tags

---
+++ src/components/submain/SidebarData.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { GiPlainCircle } from "react-icons/gi";
+export const SideBarData = [
+  {
+    icon: <GiPlainCircle color="green" size={12}/>,
+    name: "Ongoing Event",
+  },
+
+  {
+    icon: <GiPlainCircle  color= "yellow" size= {12}/>,
+    name: "Upcoming Event",
+  },
+
+  {
+    icon: <GiPlainCircle color= "red" size={12} />,
+    name: "Expired Event",
+  }
+  
+];
